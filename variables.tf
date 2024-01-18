5fc7be528f76216db26669778b9245a51a449ec7
aman <amanpatel.itofficial@gmail.com>
2024-01-18 17:10:53 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "vpc_cidr_block" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr_block" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr_block" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "availability_zone_a" {
+  description = "Availability Zone A"
+  type        = string
+}
+
+variable "availability_zone_b" {
+  description = "Availability Zone B"
+  type        = string
+}
+
